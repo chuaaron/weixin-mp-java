9a45d96d79e1c2952794abc4996fe45cfe976b32
albypeace <dudech@live.it>
2015-09-07 13:54:06 +0800

    add entity classes to last commit (INIT message)

---
+++ src/main/java/org/hamster/weixinmp/dao/entity/resp/WxRespInitEntity.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.hamster.weixinmp.dao.entity.resp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import org.hamster.weixinmp.config.WxConfig;
+import org.hamster.weixinmp.dao.entity.base.WxBaseRespEntity;
+
+
+/**
+ * @author grossopaforever@gmail.com
+ * @version Jul 28, 2013
+ * 
+ */
+@Entity
+@Table(name = WxConfig.TABLE_PREFIX + "resp_init")
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class WxRespInitEntity extends WxBaseRespEntity {
+	@Column(name = "content", length = WxConfig.COL_LEN_CONTENT, nullable = false)
+	private String content;
+	@Column(name = "lc_groupid", length = WxConfig.COL_LEN_TITLE, nullable = true)
+	private String lcGroupId;
+}
